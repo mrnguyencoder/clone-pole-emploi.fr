51ae5bfdfeb83893c94217159ab1ea3d8438cc3a
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-27 22:01:05 +0100

    add headlessui

--- src/componants/NavBar.jsx
+++ src/componants/NavBar.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { FcMenu } from "react-icons/fc";
 import { BiUser } from "react-icons/bi";
+import { FiUser } from "react-icons/fi";
 import Logo from '../assets/logo.png';
+import { Menu } from '@headlessui/react';
 
 function NavBar() {
   return (
@@ -11,8 +13,48 @@ function NavBar() {
                 <FcMenu className='text-4xl' />
                 <img src={Logo} className="h-10" alt="logo Pole Emploi" />
             </div>
-            <div className="text-4xl text-gray-600 flex justify-center items-center">
-                <BiUser />
+            <div className="text-4xl  flex justify-center items-center text-right">
+            <Menu as="div" className="relative inline-block" >
+                <Menu.Button className=""> 
+                    <BiUser className='text-gray-600'/>
+                </Menu.Button>     
+                <Menu.Items className="absolute text-slate-600 text-base text-left right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Item>
+                    {({ active }) => (
+                        
+                        <a
+                        className={`${active && 'bg-blue-500'}`}
+                        href="/"
+                        >
+                        Connection Candidate
+                        </a>
+                        
+                    )}
+                    </Menu.Item>
+                    <Menu.Item>
+                    {({ active }) => (
+                        <a
+                        className={`${active && 'bg-blue-500'}`}
+                        href="/"
+                        >
+
+                        Connection entreprise
+                        </a>
+                    )}
+                    </Menu.Item>
+                    <Menu.Item>
+                    {({ active }) => (
+                        <a
+                        className={`${active && 'bg-blue-500'}`}
+                        href="/"
+                        >
+                        Connection Particulier Employeur
+                        </a>
+                    )}
+                    </Menu.Item>
+                </Menu.Items>
+            </Menu>
+                
                 <p className="hidden md:flex md:text-lg pl-2">CONNECTION</p>
             </div>
         </div>
