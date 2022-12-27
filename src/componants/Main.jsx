499ea5842b1dbfa94e9413a68ab562467e827946
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-27 05:15:20 +0100

    rounded img

--- src/componants/Main.jsx
+++ src/componants/Main.jsx
@@ -63,47 +63,47 @@ function Main() {
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <div className="border bg-slate-100 m-8 shadow-xl">
+                <div className="border bg-slate-100 m-8 shadow-xl flex flex-col justify-between">
                    <div className="p-8 space-y-4">
                     <p className="text-3xl text-green-600 hover:text-green-500">Votre actualisation en ligne évolue pour plus de simplicité !</p>
                     <p className="text-lg text-gray-700">L’actualisation mensuelle en ligne évolue pour plus de simplicité. Retrouvez ces évolutions sur l’application mobile « Mon Espace - Pôle emploi » et sur le site de Pôle emploi.</p>
                    </div>
-                   <img src={image3} alt="" /> 
+                   <img src={image3} alt="" className='rounded-t-lg' /> 
                 </div>
-                <div className="border bg-slate-100 m-8 shadow-xl">
+                <div className="border bg-slate-100 m-8 shadow-xl flex flex-col justify-between">
                    <div className="p-8 space-y-4">
                     <p className="text-3xl text-green-600 hover:text-green-500">Métiers de la réparation d’électroménager : Fnac-Darty forme et recrute ses futurs techniciens</p>
                     <p className="text-lg text-gray-700">Saviez-vous que la réparation des appareils électroménagers est une filière d’avenir ? Pour former ses futurs techniciens réparateurs, Fnac Darty a ouvert son propre CFA à Metz Ennery. Très bientôt, d’autres CFA ouvriront à Marseille, Paris, en Ile-de- France et à Nice.</p>
                    </div>
-                   <img src={image4} alt="" /> 
+                   <img src={image4} alt="" className='rounded-t-lg' /> 
                 </div>
-                <div className="border bg-slate-100 m-8 shadow-xl">
+                <div className="border bg-slate-100 m-8 shadow-xl flex flex-col justify-between">
                    <div className="p-8 space-y-4">
                     <p className="text-3xl text-green-600 hover:text-green-500">Devenez technicien supérieur principal du développement durable !</p>
                     <p className="text-lg text-gray-700">Les inscriptions aux concours sont possibles jusqu'au 6 janvier 2023. Une fois reçu(e) au concours, vous pourrez agir pour favoriser les transports propres, aménager les villes, développer le logement, économiser l’énergie, gérer les ressources, maîtriser l’urbanisation ou prévenir les risques naturels, tout en protégeant les milieux naturels.</p>
                    </div>
-                   <img src={image5} alt="" /> 
+                   <img src={image5} alt="" className='rounded-t-lg' /> 
                 </div>
-                <div className="border bg-slate-100 m-8 shadow-xl">
+                <div className="border bg-slate-100 m-8 shadow-xl flex flex-col justify-between">
                    <div className="p-8 space-y-4">
                     <p className="text-3xl text-green-600 hover:text-green-500">Recherche d'emploi - L'aide au déplacement</p>
                     <p className="text-lg text-gray-700">Vous vous rendez à un entretien d’embauche, vous participez à un concours public, un examen certifiant ou à une immersion professionnelle ? Dans le cadre de votre recherche d’emploi vous avez peut-être droit à l’aide à la mobilité de Pôle emploi.</p>
                    </div>
-                   <img src={image6} alt="" /> 
+                   <img src={image6} alt="" className='rounded-t-lg' /> 
                 </div>
-                <div className="border bg-slate-100 m-8 shadow-xl flex-col justify-between">
+                <div className="border bg-slate-100 m-8 shadow-xl flex flex-col justify-between">
                    <div className="p-8 space-y-4">
                     <p className="text-3xl text-green-600 hover:text-green-500">12 métiers qui font bouger !</p>
                     <p className="text-lg text-gray-700">Vous aimez bouger dans votre quotidien et vous voulez qu'il en soit de même dans votre vie professionnelle ? Voici 12 idées de métiers.</p>
                    </div>
-                   <img src={image7} alt="" /> 
+                   <img src={image7} alt="" className='rounded-t-lg' /> 
                 </div>
                 <div className="border bg-slate-100 m-8 shadow-xl flex flex-col justify-between">
                    <div className="p-8 space-y-4">
                     <p className="text-3xl text-green-600 hover:text-green-500">Vivez les JOP Paris 2024 au premier rang : profitez des formations rémunérées dans la sécurité privée !</p>
                     <p className="text-lg text-gray-700">Dans le cadre des Jeux Olympiques et Paralympiques de 2024, les métiers de la sécurité recrutent fortement en Ile-de-France. 25 000 postes sont à pourvoir. Formez-vous !</p>
                    </div>
-                   <img src={image8} alt="" /> 
+                   <img src={image8} alt="" className='rounded-t-lg' /> 
                 </div>
             </div>
         </div>
