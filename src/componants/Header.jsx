c7e04d17df377a38009e88d2c78ec1f3f3014d0a
mrnguyencoder <mr.nguyencoder@gmail.com>
2022-12-27 09:18:57 +0100

    input header

--- src/componants/Header.jsx
+++ src/componants/Header.jsx
@@ -16,23 +16,26 @@ function Header() {
         style={backgroundImageStyle}>
         <div className="p-12 flex flex-col gap-5 space-y-3">
             <p className="text-4xl text-slate-50 text-center md:text-6xl">1 007 036 offres d'emploi disponibles</p>
-            <div className="flex justify-center gap-1 text-4xl md:text-6xl md:gap-0">
-                <div className="flex flex-col justify-center items-center gap-2 md:flex-row md:gap-1">
-                    <div className="flex relative items-center">
-                        <span className='absolute text-gray-400 pl-2'>
-                            <HiOutlineBriefcase className='text-2xl ' />
-                        </span>
-                        <input type="text" className='p-1 rounded-lg'/>
+            <div className="flex gap-1 md:gap-0 md:justify-center">
+                <div className="w-full md:max-w-md flex flex-col gap-2 md:flex-row md:justify-center md:gap-0">
+                    <div className="flex">
+                        <div className="text-3xl bg-white rounded-l-lg">
+                            <HiOutlineBriefcase className='h-14 text-gray-400 ml-3' />
+                        </div>
+                        <input type="text" className='w-full outline-none px-4 py-4'
+                        placeholder='Métier, compétence, mot-clé ...'
+                        />
                     </div>
-                    <div className="flex relative items-center">
-                        <span className='absolute text-gray-400 pl-2'>
-                            <HiOutlineMapPin className='text-2xl'/>
-                        </span>
-                        <input type="text"className='p-1 rounded-lg' />
+                    <div className="flex">
+                        <div className='text-3xl bg-white rounded-l-lg md:rounded-none md:border-l-2'>
+                            <HiOutlineMapPin className='h-14 text-gray-400 ml-3'/>
+                        </div>
+                        <input type="text"className='w-full outline-none px-4 py-4'
+                            placeholder='Lieu de traival' />
                     </div>
                 </div>
-                <button className='bg-red-500 rounded-md text-slate-50 shadow-md'>
-                    <HiOutlineSearch className='w-14 px-2'/>
+                <button className='bg-red-500 rounded-r-lg text-slate-50 shadow-md'>
+                    <HiOutlineSearch className='w-16 px-2'/>
                 </button>
             </div>
             <p className="text-slate-50 text-center text-base md:text-xl lg:text-3xl">Rechercher des offres par région ou par secteur d’activité</p>
